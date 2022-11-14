87523735c61b25dfd193cb77d97a30f51de54db4
Aristarh Gran <awebdeff@gmail.com>
2022-11-14 18:55:58 +0300

    add 3-4

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ console.log('randomRange(0, 10)', randomRange(0, 10))
 const rgbToHex = (r, g, b) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
 console.log('rgbToHex(0, 60, 200)', rgbToHex(0, 60, 200)) // "#003cc8"
 
+// #3 Перейти к верху страницы
+// const goTop = () => window.scrollTo(0, 0)
+// goTop()
+
+// #4 Получить количество дней между двумя датами
+const daysDiff = (d1, d2) => Math.ceil(Math.abs(d1.getTime() - d2.getTime()) / (3600 * 24 * 1000))
+console.log(daysDiff(new Date("2021-10-10"), new Date("2022-01-01"))) // 83
 
 
 
